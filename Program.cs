7d85c8f001fadfa74b5ea7e989f4ea27154d45bc
ooha20 <oohareddybirru@gmail.com>
2025-07-09 02:26:37 -0500

    Configure Render dynamic port binding

--- Program.cs
+++ Program.cs
@@ -3,44 +3,37 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+// ?? Database connection
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")
-       // sqlOpts => sqlOpts.UseIdentityColumns()   // ? forces IDENTITY(1,1)
-       //sqlServerOptions => sqlServerOptions.UseIdentityColumns()  // now available
     )
 );
 
-
-// Add services to the container.
+// ?? Add MVC services
 builder.Services.AddControllersWithViews();
-
 
 var app = builder.Build();
 
+// ?? Use PORT from environment (for Render)
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Urls.Add($"http://*:{port}");
 
-
-// Configure the HTTP request pipeline.
+// ?? Configure HTTP pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 
 app.Run();
